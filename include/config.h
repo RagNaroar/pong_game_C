f2f2f48a842fb57291113ce98cabf2dc39ab502b
RagnaR <shaxnurmurodov1990@gmail.com>
2025-12-27 23:28:46 +0500

    v0.0.1 добавляю файлы

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define FIELD_WIDTH 70
+#define FIELD_HEIGHT 22
+
+#define CHAR_TL
+#define CHAR_TR
+#define CHAR_BL
+#define CHAR_BR
+#define CHAR_H
+#define CHAR_V
+
+#endif
