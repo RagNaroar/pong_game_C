6243655c78bc910fbae4d9e7c96a4f0bf5755c0e
RagnaR <shaxnurmurodov1990@gmail.com>
2025-12-27 23:34:23 +0500

    v0.0.1 забыл добваить поле в render.c

--- include/config.h
+++ include/config.h
@@ -4,11 +4,11 @@
 #define FIELD_WIDTH 70
 #define FIELD_HEIGHT 22
 
-#define CHAR_TL
-#define CHAR_TR
-#define CHAR_BL
-#define CHAR_BR
-#define CHAR_H
-#define CHAR_V
+#define CHAR_TL '┌'
+#define CHAR_TR ' ┐ '
+#define CHAR_BL '└'
+#define CHAR_BR '┘'
+#define CHAR_H '─'
+#define CHAR_V '│'
 
 #endif
